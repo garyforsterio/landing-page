5759581d5b82754154ec5b4ce20ccae194a82538
Gary Forster <gary@garyforster.io>
2018-02-13 20:14:30 +0900

    feat: add location auto-reveal and update contact info

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 (function () {
     'use strict';
     (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
             (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date(); a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
     })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
     ga('create', 'UA-89242477-1', 'auto');
@@ -33,14 +38,43 @@
 
     //Map
     var mapToggle = document.getElementsByClassName('map__toggle')[0];
-    mapToggle.onclick = function() {
+    var debounce;
+
+    function toggleMap() {
         var elem = this;
         var map = document.getElementsByClassName('contact__map')[0];
         map.classList.toggle('displayNone');
         elem.classList.toggle('displayNone');
 
-        setTimeout(function() {
+        setTimeout(function () {
             map.classList.toggle('contact__map--hidden');
         }, 500);
     }
+
+    function isScrolledIntoView(el) {
+        var rect = el.getBoundingClientRect();
+        var elemTop = rect.top;
+        var elemBottom = rect.bottom;
+
+        var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+        return isVisible;
+    }
+
+    function mapToggleScrolledIntoView() {
+        if (isScrolledIntoView(mapToggle)) {
+            window.removeEventListener('scroll', mapToggleListener);
+            toggleMap.bind(mapToggle)();
+        }
+    }
+
+    function mapToggleListener() {
+        clearTimeout(debounce)
+        debounce = setTimeout(function () {
+            mapToggleScrolledIntoView()
+        }, 1000);
+    }
+
+    window.addEventListener('scroll', mapToggleListener);
+
+    mapToggle.onclick = toggleMap;
 })();
